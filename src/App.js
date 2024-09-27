05b9e49f33d8bd95d49c8984891a9867e4e14fd7
lilsim <87190325+lilsim@users.noreply.github.com>
2024-09-27 13:33:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './index.css';
+import Weather from './Weather.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
